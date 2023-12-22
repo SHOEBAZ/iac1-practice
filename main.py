aa48d2ab3d91f06d3ec7744caed3f7115011d995
SHOEBAZ <132372838+SHOEBAZ@users.noreply.github.com>
2023-12-22 09:46:06 +0000

    Python learning

---
+++ main.py
@@ -0,0 +1,79 @@
+wallet = 42
+
+print(wallet)
+
+wallet = 32
+
+print(wallet)
+
+day = 6
+
+weight = 87.99
+
+temp = -15 
+
+print(weight * 2)
+
+shirt = "red"
+
+print(shirt)
+
+shirt = 'I have "blue" and white shirt'
+
+print(shirt)
+
+day = 7
+month = 'october'
+temp = -15
+
+print(f"Today is {month} {day} and its {temp} degrees greater than  outside")
+
+# operators we can use <= , >=, ==, !=,
+light_is_on = False  
+
+if light_is_on:
+    print("Hello Light is on")
+else:
+    print(" It's Dark")
+
+age = 17
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Child")
+import random
+print(random.randint(1,10))
+print(random.random())
+
+# lab 7
+answer = random.randint(1,3)
+
+if answer == 1:
+    print("Yes")
+if answer == 2:
+    print("No")
+if answer == 3:
+    print("Maybe")
+# Lab 8
+import random
+lucky_number = random.randint(1,100)
+fortune_number = random.randint(1,3)
+fortune_text = ''
+
+if fortune_number == 1:
+    fortune_text = 'Tumhaara din accha rahenga :)'
+if fortune_number == 2:
+    fortune_text = 'Tumhaara din accha anhi rahenga :)'
+if fortune_number == 1:
+    fortune_text = 'Tumhaara dil bahut accha hai :)'
+print(f'{fortune_text} Your Lucky number is: {lucky_number}' )
+
+# Zero se list start hoti aur list sikhege
+
+fav_movies = ["Titanic", "Jawan", "Animal"]
+print(fav_movies[2])
+
+# lab 10 List:add, delete, insert
+
+
